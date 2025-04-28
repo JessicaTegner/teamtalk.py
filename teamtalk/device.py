f78de9a38d87c67944bb3b2419b1b3464282dd47
BlindMaster24 <375291171150z@gmail.com>
2025-04-28 17:44:03 +0300

    feat(instance): Add audio input device controls

--- teamtalk/device.py
+++ teamtalk/device.py
@@ -17,7 +17,7 @@ class SoundDevice:
         self._device_struct = device_struct
         self._is_default_input = is_default_input
 
-    def __getattr__(self, name: str):  # noqa: DAR101, DAR401
+    def __getattr__(self, name: str):
         """Gets an attribute from the underlying SDK structure.
 
         Args:
@@ -36,63 +36,12 @@ class SoundDevice:
         try:
             value = _get_tt_obj_attribute(self._device_struct, name)
             if isinstance(value, (bytes, sdk.TTCHAR, sdk.TTCHAR_P)):
-                return sdk.ttstr(value)
+                 return sdk.ttstr(value)
             return value
         except AttributeError:
             raise AttributeError(f"'{type(self).__name__}' object has no attribute '{name}'")
 
-    @property  # noqa: DAR201
-    def id(self) -> int:
-        """Gets the ID of the device.
-
-        Returns:
-            The integer device ID.
-        """
-        return self.device_id
-
-    @property  # noqa: DAR201
-    def name(self) -> str:
-        """Gets the name of the device.
-
-        Returns:
-            The string name of the device.
-        """
-        return self.device_name
-
-    @property  # noqa: DAR201
-    def sound_system(self) -> int:
-        """Gets the sound system ID (e.g., WASAPI, ALSA).
-
-        Returns:
-            The integer ID of the sound system.
-        """
-        return self.sound_system
-
-    @property  # noqa: DAR201
-    def is_input(self) -> bool:
-        """Returns True if this is an input device.
-
-        Returns:
-            True if the device has input channels, False otherwise.
-        """
-        try:
-            return self.max_input_channels > 0
-        except AttributeError:
-            return False
-
-    @property  # noqa: DAR201
-    def is_output(self) -> bool:
-        """Returns True if this is an output device.
-
-        Returns:
-            True if the device has output channels, False otherwise.
-        """
-        try:
-            return self.max_output_channels > 0
-        except AttributeError:
-            return False
-
-    @property  # noqa: DAR201
+    @property
     def is_default_input(self) -> bool:
         """Returns True if this is the default system input device.
 
@@ -101,7 +50,7 @@ class SoundDevice:
         """
         return self._is_default_input
 
-    def __repr__(self) -> str:  # noqa: DAR201
+    def __repr__(self) -> str:
         """Return a developer-friendly string representation of the device.
 
         Returns:
@@ -110,16 +59,18 @@ class SoundDevice:
         input_output = []
         default_marker = " (Default)" if self.is_default_input else ""
         try:
-            if self.is_input:
-                input_output.append("Input")
-            if self.is_output:
-                input_output.append("Output")
+            if _get_tt_obj_attribute(self._device_struct, 'max_input_channels') > 0:
+                 input_output.append("Input")
+            if _get_tt_obj_attribute(self._device_struct, 'max_output_channels') > 0:
+                 input_output.append("Output")
         except AttributeError:
-            pass
+             pass
         type_str = "/".join(input_output)
         try:
-            return f"SoundDevice(id={self.id}, name='{self.name}{default_marker}', type='{type_str}')"
-        except AttributeError:
-            _id = getattr(self._device_struct, 'nDeviceID', 'N/A')
-            _name = sdk.ttstr(getattr(self._device_struct, 'szDeviceName', 'N/A'))
+            _id = self.id
+            _name = self.name
             return f"SoundDevice(id={_id}, name='{_name}{default_marker}', type='{type_str}')"
+        except AttributeError:
+             _id_fallback = getattr(self._device_struct, 'nDeviceID', 'N/A')
+             _name_fallback = sdk.ttstr(getattr(self._device_struct, 'szDeviceName', 'N/A'))
+             return f"SoundDevice(id={_id_fallback}, name='{_name_fallback}{default_marker}', type='{type_str}')"
